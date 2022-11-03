9b84207af8b262a4bbe83328346e6ec8be51bc9a
bayaraaAPPREN <bayarsuren0310@gmail.com>
2022-11-03 14:35:50 +0800

    hahaha

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,24 +1,72 @@
 import { db } from "../connect.js"
 import bcrypt from "bcryptjs"
+import jwt from 'jsonwebtoken'
 
 
 export const login =(req,res) => {
 
+  const q = "SELECT * FROM users WHERE email = ?"
+  
+  db.query(q, [req.body.email], (err, data)=> {
+    if(err) return res.status(500).json(err);
+    if(data.length === 0) return res.status(400).json("User not found");
+
+    const checkedPassword = bcrypt.compareSync(req.body.password, data[0].password);
+
+    if(!checkedPassword) return res.status(400).json("Wrong password or email")
+    const token = jwt.sign({id:data[0].id}, "secret key")
+
+    const {password, ...others} = data[0];
+
+    res.cookie("accessToken", token,{
+      httpOnly: true
+    })
+    .status(200)
+    .json(others);
+  });
 }
+
+// export const register = (req, res) => {
+//   //CHECK USER IF EXISTS
+
+//   const q = "SELECT * FROM user WHERE email = ?";
+
+//   db.query(q, [req.body.email], (err, data) => {
+//     if (err) return res.status(500).json(err);
+//     if (data.length) return res.status(409).json("User already exists!");
+//     //CREATE A NEW USER
+//     //Hash the password
+//     const salt = bcrypt.genSaltSync(10);
+//     const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+//     const q =
+//       "INSERT INTO user (`name`,`email`,`password`) VALUE (?)";
+//     const values = [
+//       req.body.name,
+//       req.body.email,
+//       hashedPassword
+//     ];
+
+//     db.query(q, [values], (err, data) => {
+//       if (err) return res.status(500).json(err);
+//       return res.status(200).json("User has been created.");
+//     });
+//   });
+// };
 
 
 export const register =(req,res) => {
-    const q = "SELECT * FROM user WHERE name = ?"
-    db.query(q, [req.body.name], (err, data)=>{
+    const q = "SELECT * FROM users WHERE email = ?"
+    db.query(q, [req.body.email], (err, data)=>{
         if(err) return res.status(500).json(err)
-        if(data.length) return res.status(409).json("User uussen bain")
+        if(data.length) return res.status(409).json("user email already exist")
     
     //CREATE NEW USER
       //hash password
       const salt = bcrypt.genSaltSync(10);
       const hashedPassword = bcrypt.hashSync(req.body.password, salt)
 
-      const q = "INSERT INTO user (`email`, `password`,`name`) VALUE (?)"
+      const q = "INSERT INTO users (`email`, `password`,`name`) VALUE (?)"
 
       const values = [
         req.body.email, 
@@ -26,6 +74,7 @@ export const register =(req,res) => {
         req.body.name]        
 
       db.query(q, [values], (err, data) => {
+        if (err) return res.status(500).json(err);
         return res.status(200).json("User created")
       });
     });
@@ -34,5 +83,8 @@ export const register =(req,res) => {
 
 
 export const logout =(req,res) => {
-    
+    res.clearCookie("accessToken",{
+      secure:true,
+      sameSite: "none"
+    }).status(200).json("User loged out")
 }
--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import express from 'express';
 const app = express();  
-import usersRoutes from "./routes/users.js"
-import authRoutes from "./routes/auth.js"
-import examRoutes from "./routes/exam.js"
+import usersRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
+import examRoutes from "./routes/exam.js";
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
 
 
-//MIdlleware
+//MIdlleware 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", true)
+    next()
+})
 app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:3001",
+}));
+app.use(cookieParser())
 
 app.use("/api/users", usersRoutes);
 app.use("/api/auth", authRoutes);
