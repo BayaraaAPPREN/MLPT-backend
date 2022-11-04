a100ecbbc44a10a344f7ebc521a063502ddbcc4d
bayaraaAPPREN <bayarsuren0310@gmail.com>
2022-11-04 16:19:28 +0800

    haha

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,35 +26,6 @@ export const login =(req,res) => {
   });
 }
 
-// export const register = (req, res) => {
-//   //CHECK USER IF EXISTS
-
-//   const q = "SELECT * FROM user WHERE email = ?";
-
-//   db.query(q, [req.body.email], (err, data) => {
-//     if (err) return res.status(500).json(err);
-//     if (data.length) return res.status(409).json("User already exists!");
-//     //CREATE A NEW USER
-//     //Hash the password
-//     const salt = bcrypt.genSaltSync(10);
-//     const hashedPassword = bcrypt.hashSync(req.body.password, salt);
-
-//     const q =
-//       "INSERT INTO user (`name`,`email`,`password`) VALUE (?)";
-//     const values = [
-//       req.body.name,
-//       req.body.email,
-//       hashedPassword
-//     ];
-
-//     db.query(q, [values], (err, data) => {
-//       if (err) return res.status(500).json(err);
-//       return res.status(200).json("User has been created.");
-//     });
-//   });
-// };
-
-
 export const register =(req,res) => {
     const q = "SELECT * FROM users WHERE email = ?"
     db.query(q, [req.body.email], (err, data)=>{
