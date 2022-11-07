2846222d4b13a17ac25bd2572d31f549c8eee64f
bayaraaAPPREN <bayarsuren0310@gmail.com>
2022-11-07 14:10:30 +0800

    vercel ruuu shiljuullee

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin:"https://mlpt-backend.vercel.app",
+    origin:"https://mlpt.vercel.app",
   })
 );
 app.use(cookieParser());
